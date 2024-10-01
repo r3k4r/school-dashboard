a11ad45d2af5d6c6668258522f0334f584a97f4a
r3k4r <rekarjamal33@gmail.com>
2024-10-01 14:09:55 +0300

    k

--- app/(dashboard)/admin/page.jsx
+++ app/(dashboard)/admin/page.jsx
@@ -2,7 +2,18 @@
 
 const Admin = () => {
   return (
-    <div>Admin</div>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+        
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">
+
+      </div>
+      
+    </div>
   )
 }
 
